abe23a2d0ab036e1607f1aff3953ae137daaa0e0
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-12-10 19:29:04 +0530

    tissot: add prebuilt twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,5 +46,12 @@ VENDOR_SECURITY_PATCH := 2020-05-05
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+
+# Twrp
+-include $(DEVICE_PATH)/twrp/twrp.mk
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk
