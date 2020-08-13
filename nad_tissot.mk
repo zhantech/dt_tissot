e8f6f74f53995d61b3685906783b592cda3acbcb
yincen17 <yincen17@users.noreply.github.com>
2020-08-13 07:14:50 +0000

    tissot: Use Pixel Charging

--- nad_tissot.mk
+++ nad_tissot.mk
@@ -26,6 +26,7 @@ $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 $(call inherit-product-if-exists, packages/apps/NusantaraParts/nadproject.mk)
 NAD_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
+USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
