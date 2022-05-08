8cb743b9ca69ae1930b849a730dc4232047dcece
Joey Huab <joey@evolution-x.org>
2022-05-08 15:20:29 +0900

    tissot: Use TARGET_USES_MINI_GAPPS flag

--- evolution_tissot.mk
+++ evolution_tissot.mk
@@ -25,6 +25,7 @@ $(call inherit-product, device/xiaomi/tissot/device.mk)
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_MINI_GAPPS := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
