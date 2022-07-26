78b947bfc79e4f8c2be05076c7116ea399d66c7e
zhantech <hifzhan41@gmail.com>
2022-07-26 06:40:26 +0000

    tissot: BringUp RR Q

--- rr_tissot.mk
+++ rr_tissot.mk
@@ -22,16 +22,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := rr_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Faceunlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Launcher
+TARGET_LAUNCHER := lawnchair
+
+# Buildtype
+RR_BUILDTYPE := Milestone
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
