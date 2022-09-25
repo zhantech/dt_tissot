697ccb1867c5de650b0a67f02a59f83f564a70d2
zhantech <hifzhan41@gmail.com>
2022-09-25 16:59:35 +0700

    tissot: BringUp RiceDroid

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -1,18 +1,28 @@
 #
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
+
+# Inherit some common RiceDroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
@@ -30,3 +40,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+# RiceDroid Stuffs
+RICE_MAINTAINER := ZHANtech™
+RICE_DEVICE := tissot
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_AOSP_CLOCK := true
+SUSHI_BOOTANIMATION := 720
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_QUICK_TAP := true
