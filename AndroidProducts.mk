3fb8fc31e2c144d0bcf74eb76604b7be13f2cc7c
zhantech <hifzhan41@gmail.com>
2022-09-28 16:57:05 +0700

    tissot: BringUp RiceDroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_tissot.mk
+    $(LOCAL_DIR)/lineage_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_tissot-user \
-    nad_tissot-userdebug \
-    nad_tissot-eng
+    lineage_tissot-user \
+    lineage_tissot-userdebug \
+    lineage_tissot-eng
